8f78b942eb730491432dbd6724e054e0b8fd3134
Jeremy Ellis <keyfreemusic@gmail.com>
2022-12-20 11:14:55 -0800

    Update portenta-socket.ino

--- portenta-socket.ino
+++ portenta-socket.ino
@@ -1,149 +1,113 @@
-/*
- * socket.ino
- * 
- * created 13 July 2010
- * by dlf (Metodo2 srl)
- * modified 31 May 2012
- * by Tom Igoe
- * 
- * modified By Jeremy Ellis 
- * twitter @rocksetta
- * Webpage http://rocksetta.com
- * Arduino High School Robotics Course at
- * https://github.com/hpssjellis/arduino-high-school-robotics-course
- * 
- *  Update Feb 12th, 2020
- *  Specifically for the Arduino Nano 33 IoT
- *
- */
+//    https://www.amebaiot.com/zh/rtl8195-arduino-api-wifisslclient/
+
+//    example at   https://github.com/ambiot/amb1_arduino/blob/dev/Arduino_package/hardware/libraries/WiFi/examples/WiFiSSLClient/WiFiSSLClient.ino
 
 
-#include <SPI.h>
-#include <WiFiNINA.h>
+#include <WiFi.h>
+#include <WiFiSSLClient.h>
 
-#include "arduino_secrets.h" 
-///////please enter your sensitive data in the Secret tab/arduino_secrets.h
-char ssid[] = SECRET_SSID;        // your network SSID (name)
-char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
-int keyIndex = 0;            // your network key Index number (needed only for WEP)
+
+char ssid[] = "";        // your network SSID (name)
+char pass[] = "";       // your network password (use for WPA, or use as key for WEP)
+int keyIndex = 0;                   // your network key Index number (needed only for WEP)
 
 int status = WL_IDLE_STATUS;
-// if you don't want to use DNS (and reduce your sketch size)
-// use the numeric IP instead of the name for the server:
-//IPAddress server(74,125,232,128);  // numeric IP for Google (no DNS)
 
+char HTTPS_SERVER[] = "enfqyo-8080.preview.csb.app";
+char HTTPS_PATH[] = "/";
 
-///////////////////////// Node Websocket server url without http:// or ending /         ////////
+// no certificate needed, Arduino has made it already a part of the SSLclient.
 
-//   so https://myURL/
-
-//   becomes
-
-//   myURL
-
-char server[] = "8080-e71e03c0-51bc-4fc8-8019-df8863ff0ce8.ws-us02.gitpod.io";    
-
-///////////////////////// above is important  ////////////////////////////////////////////////////
-
-String myRandWebSocket = String(rand()*10000+10000); //attempt at random security
-
-
-// Initialize the Ethernet client library
-// with the IP address and port of the server
-// that you want to connect to (port 80 is default for HTTP):
-//WiFiClient client;
 WiFiSSLClient client;
+unsigned long myStore;
+
+
 
 void setup() {
-  pinMode(LED_BUILTIN,OUTPUT);
-  //Initialize serial and wait for port to open:
-  Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
+    //Initialize serial and wait for port to open
+    Serial.begin(115200);
 
-  // check for the WiFi module:
-  if (WiFi.status() == WL_NO_MODULE) {
-    Serial.println("Communication with WiFi module failed!");
-    // don't continue
-    while (true);
-  }
+    // attempt to connect to Wifi network
+    while (status != WL_CONNECTED) {
+        Serial.print("\r\n Attempting to connect to SSID: ");
+        Serial.println(ssid);
+        // Connect to WPA/WPA2 network. Change this line if using open or WEP network
+        status = WiFi.begin(ssid,pass);
 
-  String fv = WiFi.firmwareVersion();
-  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
-    Serial.println("Please upgrade the firmware");
-  }
+        // wait for connection
+        delay(5000);
+    }
+    Serial.println("Connected to wifi");
+    printWifiStatus();
 
-  // attempt to connect to Wifi network:
-  while (status != WL_CONNECTED) {
-    Serial.print("Attempting to connect to SSID: ");
-    Serial.println(ssid);
-    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
-    status = WiFi.begin(ssid, pass);
+    Serial.println("\nStarting connection to server...");
+ // client.setRootCA((unsigned char*)rootCABuff);  // already done by arduino
+    // if a connection is formed, report back via serial
+    if (client.connect(HTTPS_SERVER, 443)) {
+        Serial.println("connected to server");
+        // Make a HTTP request:
+        client.print("GET ");
+        client.print(HTTPS_PATH);
+        client.println(" HTTP/1.1");
+        client.print("Host: ");
+        client.println(HTTPS_SERVER);
+        client.println("Upgrade: websocket");
+        client.println("Connection: Upgrade");
+        client.println("Sec-WebSocket-Key: x3JJHMbDL1EzLkh9GBhXDw==");
+        client.println("Sec-WebSocket-Protocol: chat, superchat");
+        client.println("Sec-WebSocket-Version: 13");
+       // client.println("Origin: http://example.com");
+        client.println();
+    } else {
+        Serial.println("connected to server failed");
+    }
+    delay(100);
 
-    // wait 10 seconds for connection:
-    delay(10000);
-  }
-  Serial.println("Connected to wifi");
-  printWifiStatus();
-
-  Serial.println("\nStarting connection to server...");
-  // if you get a connection, report back via serial:  
-  //if (client.connect(server, 80)) {
-  if (client.connect(server, 443)) {
-    Serial.println("connected to server");
-    // Make a HTTP request:
-    client.println("GET / HTTP/1.1");
-    client.println("Host: "+String(server));
-    client.println("Upgrade: websocket");
-    client.println("Connection: Upgrade");
-    client.println("Sec-WebSocket-Key: dGhlIHNhbXBsZSBub25jZQ==");
-    client.println("Sec-WebSocket-Version: 13");
-    client.println();   
-  }
+    myStore = millis();
 }
 
 void loop() {
-  // if there are incoming bytes available
-  // from the server, read them and print them:
-  while (client.available()) {
-    char c = client.read();
-    Serial.write(c);
-    if (c == 'A') {                    // can use any single letter or character
-       digitalWrite(LED_BUILTIN, 1);               
-       Serial.println();
-    }    
-    if (c == 'B') {
-       digitalWrite(LED_BUILTIN, 0);               
-       Serial.println();
+   
+    // if there are incoming bytes available
+    // from the server, read them and print them
+    while (client.available()) {
+        char c = client.read();
+        Serial.write(c);
     }
-  }
 
-  // if the server's disconnected, stop the client:
-  if (!client.connected()) {
-    Serial.println();
-    Serial.println("disconnecting from server.");
-    client.stop();
+    if ((millis() - myStore) >= 10000 ) {
+      myStore = millis();
+      Serial.println("Send: D somehow"); 
+     // client.print((char)0x00); // Start of message frame
+     // client.print("D");    // Message payload
+     // client.print((char)0xff); // End of message frame
+    }
 
-    // do nothing forevermore:
-    while (true);
-  }
+
+    // if the server's disconnected, stop the client
+    if (!client.connected()) {
+        Serial.println();
+        Serial.println("disconnecting from server.");
+        client.stop();
+
+        // do nothing
+        while (true);
+    }
 }
 
-
 void printWifiStatus() {
-  // print the SSID of the network you're attached to:
-  Serial.print("SSID: ");
-  Serial.println(WiFi.SSID());
+    // print the SSID of the network you're attached to:
+    Serial.print("SSID: ");
+    Serial.println(WiFi.SSID());
 
-  // print your board's IP address:
-  IPAddress ip = WiFi.localIP();
-  Serial.print("IP Address: ");
-  Serial.println(ip);
+    // print your WiFi shield's IP address:
+    IPAddress ip = WiFi.localIP();
+    Serial.print("IP Address: ");
+    Serial.println(ip);
 
-  // print the received signal strength:
-  long rssi = WiFi.RSSI();
-  Serial.print("signal strength (RSSI):");
-  Serial.print(rssi);
-  Serial.println(" dBm");
+    // print the received signal strength:
+    long rssi = WiFi.RSSI();
+    Serial.print("signal strength (RSSI):");
+    Serial.print(rssi);
+    Serial.println(" dBm");
 }
